ff08c8bac42f3471e0b23c9ba91660bed36528ce
edo masaru <masaruedo@yahoo.com>
2015-12-26 18:43:50 +0700

    Add a little style to dropzone

--- application/views/index.php
+++ application/views/index.php
@@ -3,11 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Codeigniter+DropzoneJs</title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>vendor/dropzone/dropzone.min.css">
+	<style>
+		body {
+			background: #f7f7f7;
+			font-family: 'Montserrat', sans-serif;
+		}
+
+		.dropzone {
+			background: #fff;
+			border: 2px dashed #ddd;
+			border-radius: 5px;
+		}
+
+		.dz-message {
+			color: #999;
+		}
+
+		.dz-message:hover {
+			color: #464646;
+		}
+
+		.dz-message h3 {
+			font-size: 200%;
+			margin-bottom: 15px;
+		}
+	</style>
 </head>
 <body>
 	<div id="content">
-		<div id="my-dropzone" class="dropzone"></div>
+		<div id="my-dropzone" class="dropzone">
+			<div class="dz-message">
+				<h3>Drop files here</h3> or <strong>click</strong> to upload
+			</div>
+		</div>
 	</div>
 
 	<script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
